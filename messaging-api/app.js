0a55d47d11d151bf27d4e78d2ef3b595cf3fc231
Bill Sorensen <baldgoth@gmail.com>
2019-03-28 17:03:43 -0500

    Add CORS support to API

--- messaging-api/app.js
+++ messaging-api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const nodeCleanup = require('node-cleanup');
+const cors = require('cors');
 
 const sourceRouter = require('./routes/source');
 const messageRouter = require('./routes/message');
@@ -11,6 +12,11 @@ const app = express();
 const port = 8880; // TODO move to config
 
 app.use(logger('dev'));
+
+// NOTE: Currently allowing any origin - lock this down
+// or remove it if can host both on same site
+// NOTE: Will need to allow pre-flight if use DELETE
+app.use(cors());
 
 app.use('/source', sourceRouter);
 app.use('/message', messageRouter);
