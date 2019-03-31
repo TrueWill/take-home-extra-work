0e44d4001421a3387fccb6ccea10a49146a1ddba
Bill Sorensen <baldgoth@gmail.com>
2019-03-31 10:49:04 -0500

    Convert App to function component

--- messaging/src/App.js
+++ messaging/src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Sources from './Sources/SourcesContainer.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Messaging</h1>
-        <Sources />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Messaging</h1>
+      <Sources />
+    </div>
+  );
 }
 
 export default App;
